418c09f33d25b8a39dabeccc1a84abd4e70e08e9
arodriguez <arodriguez@binovo.es>
2022-12-02 14:36:05 +0100

    [FIX] l10n_es_pos: Clear orders already registered in the backend

--- l10n_es_pos/static/src/js/models.js
+++ l10n_es_pos/static/src/js/models.js
@@ -9,12 +9,21 @@ odoo.define('l10n_es_pos.models', function (require) {
     var logger = require('point_of_sale.logger');
     var models = require('point_of_sale.models');
 
+    models.load_fields('pos.config', ['l10n_es_last_pos_order']);
+
     var pos_super = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
         initialize: function (attributes, options) {
             pos_super.initialize.apply(this, arguments);
             this.pushed_simple_invoices = [];
             return this;
+        },
+        after_load_server_data: function() {
+            var self = this;
+            var orders = this.db.get_orders();
+            var resIndex = orders.findIndex((p) => p.data.name == this.config.l10n_es_last_pos_order);
+            orders.slice(0, resIndex + 1).forEach(o => self.db.remove_order(o.id));
+            return pos_super.after_load_server_data.call(this);
         },
         get_simple_inv_next_number: function () {
             return this.config.l10n_es_simplified_invoice_prefix+this.get_padding_simple_inv(this.config.l10n_es_simplified_invoice_number);
