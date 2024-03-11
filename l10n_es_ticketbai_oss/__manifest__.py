ffc831c11b3e9b5fe7314051699d872d6eea432b
arodriguez <arodriguez@binovo.es>
2024-03-11 13:20:39 +0100

    [ADD] l10n_es_ticketbai_oss: Add OSS support for TicketBai

---
+++ l10n_es_ticketbai_oss/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Binovo IT Human Project SL
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "TicketBAI - OSS Support ",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "http://www.binovo.es",
+    "author": "Binovo",
+    "license": "AGPL-3",
+    "development_status": "alpha",
+    "maintainers": ["Binovo"],
+    "depends": ["l10n_es_ticketbai"],
+    "data": ["views/account_tax_views.xml"],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
